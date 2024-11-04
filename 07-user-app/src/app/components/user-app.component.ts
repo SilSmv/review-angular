2217736ea3dfef1d7c66c9c22a8fd42638cf3cdb
Silvana <silvana.munoz@ucb.edu.bo>
2024-11-04 19:04:28 -0400

    Create new project user-app

---
+++ 07-user-app/src/app/components/user-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-app.component.html'
+})
+export class UserAppComponent {
+
+}
