dc0220cf008ba94e97ac34f166289ed8c3c59f41
Silvana <silvana.munoz@ucb.edu.bo>
2024-11-05 00:33:14 -0400

    routing

---
+++ 09-user-app-routing-param/src/app/components/user-app.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import Swal from 'sweetalert2';
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SharingDataService } from '../services/sharing-data.service';
+
+@Component({
+  selector: 'user-app',
+  standalone: true,
+  imports: [RouterOutlet,NavbarComponent],
+  templateUrl: './user-app.component.html'
+})
+export class UserAppComponent implements OnInit{
+  title: string = 'Listado de usuarios';
+
+  users: User[] = [];
+
+  constructor(
+    private service: UserService,
+    private sharingData: SharingDataService,
+    private router:Router
+  ){
+
+
+  }
+  ngOnInit(): void {
+    this.service.findAll().subscribe(users  => this.users = users)
+    this.addUser();
+    this.removeUser();
+    this.findUserById();
+    
+  }
+  findUserById(){
+    this.sharingData.findUserByIdEventEmitter.subscribe(id => {
+      const user  = this.users.find(user => user.id = id)
+      this.sharingData.selectUserEventEmitter.emit(user);
+    })
+
+  }
+  addUser(){
+    this.sharingData.newUserEventEmitter.subscribe(user=>{
+      if(user.id>0){
+        this.users = this.users.map(u => (u.id === user.id)?{... user}:u)
+        
+      }else{
+        this.users = [... this.users,{...user,id:new Date().getTime()}];
+      }
+      this.router.navigate(['/users'], {state: {users:this.users}});
+      Swal.fire({
+        title: "Guardado!",
+        text: "Se ha guardado el elemento!",
+        icon: "success"
+      });
+
+    })
+  
+  }
+  removeUser(){
+    this.sharingData.idUserEventEmitter.subscribe(id =>{
+
+      Swal.fire({
+        title: "Esta seguro que quiere eliminar?",
+        text: "Cuidado el dato del sistema sera eliminado!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.users = this.users.filter(user => user.id!==id);
+          this.router.navigate(['/users/create'],{skipLocationChange:true}).then(() =>{
+            this.router.navigate(['/users'], {state: {users:this.users}});
+          })
+
+          Swal.fire({
+            title: "Eliminado!",
+            text: "Usuario eliminado con exito .",
+            icon: "success"
+          });
+        }
+      });
+
+    })
+   
+
+ 
+  }
+
+
+}
---
+++ 09-user-app-routing-param/src/app/components/user/user.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../models/user';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { SharingDataService } from '../../services/sharing-data.service';
+
+@Component({
+  selector: 'user',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './user.component.html'
+})
+export class UserComponent {
+  title: string = 'Listado de usuarios';
+  users:User[] = []
+
+
+  constructor(private sharingData: SharingDataService,
+    private service: UserService, private router:Router){
+    if(this.router.getCurrentNavigation()?.extras.state){
+      this.users = this.router.getCurrentNavigation()?.extras.state!['users'];
+    }else{
+      this.service.findAll().subscribe(users => this.users = users)
+    }
+  }
+
+  onRemoveUser(id:number): void{
+     this.sharingData.idUserEventEmitter.emit(id)
+
+
+  }
+  onSelectedUser(user:User):void{
+    this.router.navigate(['/users/edit',user.id],{state:{user}});
+  }
+
+}
